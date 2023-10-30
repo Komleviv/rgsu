79e7b72222fba9e48f1f0fb174e3e46430764ace
Komlev Ivan <komleviv@gmail.com>
2023-10-30 10:05:28 +0300

    Исправлена ошибка с пустыми данными о владельцах

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 require_once 'src/database.php';
 require_once 'src/user.php';
+require_once 'src/file.php';
+
+$user = new User();
+$isLogin = false;
 
 if (isset($_POST['login'], $_POST['pass'])) {
-    $user = new User();
     $isLogin = $user->authorization($_POST['login'], $_POST['pass']);
     if ($isLogin)
         echo "Пользователь авторизован.";
+}
+
+if (isset($_FILES['xml'])) {
+    $file = new File();
+    $xmlArray = $file->xmlParse($_FILES['xml']['tmp_name']);
 }
 
 ?>
@@ -23,11 +31,36 @@ if (isset($_POST['login'], $_POST['pass'])) {
 </head>
 <body>
     <div class="container">
-        <form action="index.php" method="post">
-            <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
-            <input type="text" class="form-control" name="pass" id="pass" placeholder="Введите пароль"><br>
-            <button class="btn btn-success" type="submit">Авторизоваться</button>
-        </form>
+        <?php
+            if (isset($_COOKIE['user']) || $isLogin):
+        ?>
+            <form action="index.php" method="post" enctype="multipart/form-data">
+                <label for="xml">Выберите XML-файл для загрузки:</label>
+                <input type="file" id="xml" name="xml" accept=".xml" />
+                <button class="btn btn-success" type="submit">Загрузить XML</button>
+            </form>
+        <?php
+            else:
+        ?>
+            <form action="index.php" method="post">
+                <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
+                <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль"><br>
+                <button class="btn btn-success" type="submit">Авторизоваться</button>
+            </form>
+        <?php
+            endif;
+        ?>
+    </div>
+    <div class="table">
+        <h5>Люди, у которых есть питомцы старше 3 лет:</h5>
+        <?php
+            $ownersName = $user->getUserQuery();
+            if(isset($ownersName)) {
+                foreach ($ownersName as $name) {
+                    echo $name;
+                }
+            }
+        ?>
     </div>
 </body>
 </html>
