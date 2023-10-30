aafeef81c63cbf0bb95d1360ef620e4899efd235
Komlev Ivan <komleviv@gmail.com>
2023-10-31 02:09:51 +0300

    Добавлен функционал регистрации пользователем

--- index.php
+++ index.php
@@ -4,17 +4,32 @@ require_once 'src/user.php';
 require_once 'src/file.php';
 
 $user = new User();
+$infoString = "";
 $isLogin = false;
+$isRegister = false;
 
 if (isset($_POST['login'], $_POST['pass'])) {
-    $isLogin = $user->authorization($_POST['login'], $_POST['pass']);
-    if ($isLogin)
-        echo "Пользователь авторизован.";
+    if (isset($_GET['reg'])) {
+        $regStatus = $user->registration($_POST['name'], $_POST['login'], $_POST['pass']);
+        $isRegister = $regStatus[0];
+        $infoString = $regStatus[1];
+    } else {
+        $isLogin = $user->authorization($_POST['login'], $_POST['pass']);
+        if ($isLogin)
+            $infoString = "Пользователь авторизован.";
+    }
 }
 
 if (isset($_FILES['xml'])) {
     $file = new File();
-    $xmlArray = $file->xmlParse($_FILES['xml']['tmp_name']);
+
+    if (isset($_COOKIE['user'])) {
+        $xmlArray = $file->xmlParseAndQuery($_FILES['xml']['tmp_name']);
+
+        if ($xmlArray) {
+            $infoString = "Данные из XML успешно добавлены в базу данных.";
+        }
+    }
 }
 
 ?>
@@ -24,43 +39,67 @@ if (isset($_FILES['xml'])) {
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&subset=latin,cyrillic" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="src/style.css" type="text/css">
     <title>Список животных</title>
 </head>
-<body>
+<body class="body-bg">
     <div class="container">
-        <?php
-            if (isset($_COOKIE['user']) || $isLogin):
-        ?>
-            <form action="index.php" method="post" enctype="multipart/form-data">
-                <label for="xml">Выберите XML-файл для загрузки:</label>
-                <input type="file" id="xml" name="xml" accept=".xml" />
-                <button class="btn btn-success" type="submit">Загрузить XML</button>
-            </form>
-        <?php
-            else:
-        ?>
-            <form action="index.php" method="post">
-                <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
-                <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль"><br>
-                <button class="btn btn-success" type="submit">Авторизоваться</button>
-            </form>
-        <?php
-            endif;
-        ?>
-    </div>
-    <div class="table">
-        <h5>Люди, у которых есть питомцы старше 3 лет:</h5>
-        <?php
-            $ownersName = $user->getUserQuery();
-            if(isset($ownersName)) {
-                foreach ($ownersName as $name) {
-                    echo $name;
+        <div class="alert alert-success">
+            <?= $infoString ?>
+        </div>
+        <?php if (isset($_COOKIE['user']) || $isLogin || $isRegister): ?>
+            <div class="xml-parsing">
+                <form action="index.php" method="post" enctype="multipart/form-data">
+                    <label for="xml">Выберите XML-файл для загрузки:</label>
+                    <input type="file" id="xml" name="xml" accept=".xml" />
+                    <button class="btn btn-success" type="submit">Загрузить XML</button>
+                </form>
+            </div>
+        <?php else: ?>
+            <div class="row">
+                <div class="col" style="width: 25em">
+                    <form action="index.php?reg=1" method="post">
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Укажите своё имя"><br>
+                        <input type="text" class="form-control" name="login" id="login" placeholder="Придумайте логин"><br>
+                        <input type="password" class="form-control" name="pass" id="pass" placeholder="Придумайте пароль"><br>
+                        <button class="btn btn-success" type="submit">Зарегистрироваться</button>
+                    </form>
+                </div>
+                <div class="col" style="width: 25em">
+                    <form action="index.php" method="post">
+                        <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
+                        <input type="password" class="form-control" name="pass" id="pass" placeholder="Введите пароль"><br>
+                        <button class="btn btn-success" type="submit">Авторизоваться</button>
+                    </form>
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="owner-pets">
+            <h5>Люди, у которых есть питомцы старше 3 лет:</h5>
+            <div class="card-body">
+                <?php
+                $ownersName = $user->getOwnerQuery();
+                if(isset($ownersName)) {
+                    foreach ($ownersName as $owner) {
+                ?>
+                <div class="row">
+                    <div class="col">
+                        <?= $owner['name'] ?>
+                    </div>
+                    <div class="col">
+                        <?= $owner['nickname'] ?>
+                    </div>
+                    <div class="col">
+                        <?= $owner['age'] ?>
+                    </div>
+                </div>
+                <?php
+                    }
                 }
-            }
-        ?>
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
--- src/user.php
+++ src/user.php
@@ -2,9 +2,63 @@
 require_once __DIR__ . '\database.php';
 
 class User {
+    public $name;
     public $login;
     private $password;
 
+    /**
+     * Метод фильтрует, проверяет и записывает в базу данных данные, введённые в форму регистрации
+     * @param string $name
+     * @param string $login
+     * @param string $pass
+     * @return array
+     */
+    public function registration(string $name, string $login, string $pass) : array
+    {
+        $this->name = filter_var(trim($name), FILTER_UNSAFE_RAW);
+        $this->login = filter_var(trim($login), FILTER_UNSAFE_RAW);
+        $this->password = filter_var(trim($pass), FILTER_UNSAFE_RAW);
+
+        if(mb_strlen($this->name) < 4 || mb_strlen($this->name) > 32)
+        {
+            echo "Недопустимая длина логина. Логин должен содержать от 4 до 16 символов.";
+            exit();
+        }
+        if(mb_strlen($this->login) < 4 || mb_strlen($this->login) > 16)
+        {
+            echo "Недопустимая длина логина. Логин должен содержать от 4 до 16 символов.";
+            exit();
+        }
+        if(mb_strlen($this->password) < 5 || mb_strlen($this->password) > 32)
+        {
+            echo "Недопустимая длина пароля. Пароль должен содержать от 5 до 32 символов.";
+            exit();
+        }
+
+        $this->password = md5($this->password."rgsurgsu123");
+
+        $db = new DataBase();
+        $sql = "SELECT login FROM users WHERE login = '$this->login'";
+        $loginName = $db->dbSelectQuery($sql);
+        if (!empty($loginName))
+        {
+            return array(false ,"Пользователь с таким логином уже зарегистрирован в системе.");
+        }
+
+        $sql = "INSERT INTO users (name, login, password) VALUES ('$this->name', '$this->login', '$this->password')";
+        $db->dbQuery($sql);
+
+        setcookie('user', $this->login, time() + 3600, "/");
+
+        return array(true, "Пользователь зарегистрирован и авторизован.");
+    }
+
+    /**
+     * Метод фильтрует и проверяет данные, введённые в форму авторизации
+     * @param string $login
+     * @param string $pass
+     * @return array
+     */
     public function authorization(string $login, string $pass) : bool
     {
         $this->login = filter_var(trim($login), FILTER_UNSAFE_RAW);
@@ -28,7 +82,7 @@ class User {
         $loginName = $db->dbSelectQuery($sql);
 
         if (isset($loginName)) {
-            setcookie('user', $loginName['login'], time() + 3600, "/");
+            setcookie('user', $loginName[0]['login'], time() + 3600, "/");
 
             return true;
         }
@@ -36,15 +90,20 @@ class User {
         return false;
     }
 
+    /**
+     * Метод получения из базы данных информации о хозяевах с животными старше указанного возраста (по умолчанию 3 года)
+     * @param int $year
+     * @return array|null
+     */
     public function getOwnerQuery(int $year = 3) : array|null
     {
         $db = new DataBase();
-        $sql = "SELECT u.name FROM owners u 
+        $sql = "SELECT u.name, p.nickname, p.age FROM owners u 
                 LEFT JOIN owners_pets up ON up.owner_id = u.id 
                 LEFT JOIN pets p ON up.pet_id = p.id 
                 WHERE p.age > '$year'";
-        $usersName = $db->dbSelectQuery($sql);
+        $result = $db->dbSelectQuery($sql);
 
-        return $usersName;
+        return $result;
     }
 }
