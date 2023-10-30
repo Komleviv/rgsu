3848b2f2881f6582bed994b3acb10a8cd8e750c6
Komlev Ivan <komleviv@gmail.com>
2023-10-30 18:51:25 +0300

    Изменена структура БД

--- rgsu.sql
+++ rgsu.sql
@@ -10,6 +10,17 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `rgsu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `rgsu`;
 
+CREATE TABLE `owners` (
+  `id` int NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `owners_pets` (
+  `id` int NOT NULL,
+  `owner_id` int NOT NULL,
+  `pet_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE `pets` (
   `id` int NOT NULL,
   `code` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -21,20 +32,19 @@ CREATE TABLE `pets` (
 
 CREATE TABLE `pets_relatives` (
   `id` int NOT NULL,
-  `parent_id` int NOT NULL,
-  `children_id` int NOT NULL
+  `parent_code` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `children_code` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `pets_rewards` (
   `id` int NOT NULL,
   `pet_id` int NOT NULL,
-  `rewards_id` int NOT NULL
+  `reward_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `rewards` (
   `id` int NOT NULL,
-  `reward_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `show_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `types_breeds` (
@@ -45,18 +55,19 @@ CREATE TABLE `types_breeds` (
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `owner_id` int NOT NULL,
   `login` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `role` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `users_pets` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `pet_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+ALTER TABLE `owners`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `owners_pets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `owners_pets_ibfk_1` (`pet_id`),
+  ADD KEY `owner_id` (`owner_id`);
 
 ALTER TABLE `pets`
   ADD PRIMARY KEY (`id`),
@@ -65,13 +76,13 @@ ALTER TABLE `pets`
 
 ALTER TABLE `pets_relatives`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `pets_relatives_ibfk_1` (`parent_id`),
-  ADD KEY `children_id` (`children_id`);
+  ADD KEY `pets_relatives_ibfk_2` (`children_code`),
+  ADD KEY `parent_code` (`parent_code`);
 
 ALTER TABLE `pets_rewards`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `pets_rewards_ibfk_1` (`pet_id`),
-  ADD KEY `rewards_id` (`rewards_id`);
+  ADD KEY `pets_rewards_ibfk_3` (`pet_id`),
+  ADD KEY `reward_id` (`reward_id`);
 
 ALTER TABLE `rewards`
   ADD PRIMARY KEY (`id`);
@@ -83,13 +94,8 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `login` (`login`);
 
-ALTER TABLE `users_pets`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `users_pets_ibfk_1` (`pet_id`),
-  ADD KEY `users_pets_ibfk_2` (`user_id`);
 
-
-ALTER TABLE `pets`
+ALTER TABLE `owners_pets`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `pets_relatives`
@@ -98,33 +104,21 @@ ALTER TABLE `pets_relatives`
 ALTER TABLE `pets_rewards`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `rewards`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `types_breeds`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `users_pets`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `owners_pets`
+  ADD CONSTRAINT `owners_pets_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`),
+  ADD CONSTRAINT `owners_pets_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `owners` (`id`);
 
 ALTER TABLE `pets`
   ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`breed_name_id`) REFERENCES `types_breeds` (`id`);
 
 ALTER TABLE `pets_relatives`
-  ADD CONSTRAINT `pets_relatives_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `pets` (`id`),
-  ADD CONSTRAINT `pets_relatives_ibfk_2` FOREIGN KEY (`children_id`) REFERENCES `pets` (`id`);
+  ADD CONSTRAINT `pets_relatives_ibfk_2` FOREIGN KEY (`children_code`) REFERENCES `pets` (`code`),
+  ADD CONSTRAINT `pets_relatives_ibfk_3` FOREIGN KEY (`parent_code`) REFERENCES `pets` (`code`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 ALTER TABLE `pets_rewards`
-  ADD CONSTRAINT `pets_rewards_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`),
-  ADD CONSTRAINT `pets_rewards_ibfk_2` FOREIGN KEY (`rewards_id`) REFERENCES `rewards` (`id`);
-
-ALTER TABLE `users_pets`
-  ADD CONSTRAINT `users_pets_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`),
-  ADD CONSTRAINT `users_pets_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `pets_rewards_ibfk_3` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`),
+  ADD CONSTRAINT `pets_rewards_ibfk_4` FOREIGN KEY (`reward_id`) REFERENCES `rewards` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
